03c891bff5046ed3760d99f49267b69cfeb2a62c
Jose Miguel Montanana <jmmontanana@gmail.com>
2018-01-19 16:55:27 +0100

    Adding new functionalities as repository of files

---
+++ Server/app_with_repository.js
@@ -0,0 +1,98 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var os = require("os");
+var elasticsearch = require('elasticsearch');
+var elastic = new elasticsearch.Client({
+  host: 'localhost:9400',
+  log: 'error'
+});
+
+/* monitoring routes */
+var routes      = require('./routes/v1/index');
+var repository  = require('./routes/v1/repository');
+var workflows   = require('./routes/v1/workflows');
+var devices     = require('./routes/v1/devices'); /*2017*/
+var summary     = require('./routes/v1/summary'); /*2017*/
+var deployment = require('./routes/v1/deployment'); /*2017*/
+var deploymentcomm = require('./routes/v1/deploymentcomm'); /*2017*/
+var deploymentcomp = require('./routes/v1/deploymentcomp'); /*2017*/
+var experiments = require('./routes/v1/experiments');
+var metrics     = require('./routes/v1/metrics');
+var profiles    = require('./routes/v1/profiles');
+var runtime     = require('./routes/v1/runtime');
+var statistics  = require('./routes/v1/statistics');
+
+/* resource manager routes */
+var configs = require('./routes/v1/configs');
+var resources = require('./routes/v1/resources');
+
+var app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.set('elastic', elastic);
+app.set('version', '02.02.17');
+var port = '3033',
+  hostname = os.hostname();
+// redirect backend hostname to front-end
+//hostname = hostname.replace('be.excess-project.eu', 'mf.excess-project.eu');
+app.set('mf_server', 'http://' + hostname + ':' + port + '/v1');
+//app.set('pwm_idx', 'power_dreamcloud');
+
+app.use(logger('combined', {
+  skip: function (req, res) { return res.statusCode < 400; }
+}));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+/* monitoring URL paths */
+app.use('/', routes);
+app.use('/v1/phantom_mf', routes);
+app.use('/v1/phantom_mf/repository', repository);
+app.use('/v1/phantom_mf/workflows', workflows);
+app.use('/v1/phantom_mf/experiments', experiments);
+app.use('/v1/phantom_mf/devices',devices); /*2017*/
+app.use('/v1/phantom_mf/summary',summary); /*2017*/
+app.use('/v1/phantom_mf/deployment',deployment); /*2017*/
+app.use('/v1/phantom_mf/deploymentcomm',deploymentcomm); /*2017*/
+app.use('/v1/phantom_mf/deploymentcomp',deploymentcomp); /*2017*/
+
+
+app.use('/v1/phantom_mf/metrics', metrics);
+app.use('/v1/phantom_mf/profiles', profiles);
+app.use('/v1/phantom_mf/runtime', runtime);
+app.use('/v1/phantom_mf/statistics', statistics);
+
+/*resource manager URL paths */
+app.use('/v1/phantom_rm/resources', resources);
+app.use('/v1/phantom_rm/configs', configs);
+
+/* catch 404 and forward to error handler */
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  next(err);
+});
+
+// development error handler
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    var error = {};
+    error.error = err;
+    res.json(error);
+  });
+}
+
+// production error handler
+app.use(function(err, req, res, next) {
+  var error = {};
+  error.error = err;
+  res.json(error);
+});
+
+module.exports = app;
---
+++ Server/start_with_repository.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+#  Copyright (C) 2016 University of Stuttgart
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+cp package_with_repository.json package.json
+#
+# GLOBAL VARIABLES
+#
+BASE_DIR=`pwd`
+TMP_DIR=${BASE_DIR}/tmp
+DIST_DIR=${BASE_DIR}/dist
+
+#
+# CHECK INSTALLATION
+#
+mkdir -p ${TMP_DIR}
+
+echo "Checking ..."
+echo "> elasticsearch"
+
+ES_HOME=${DIST_DIR}/elasticsearch
+ELASTICSEARCH_BIN=${ES_HOME}/bin/elasticsearch
+cp ${BASE_DIR}/elasticsearch.yml ${ES_HOME}/config/elasticsearch.yml
+elasticsearch_port="9400";
+nodejs_port=3033
+
+command -v ${ELASTICSEARCH_BIN} >/dev/null 2>&1 || { echo " elasticsearch : Not installed. Aborting." >&2; exit 1; }
+
+RESULT=$(netstat -lnt | awk '$6 == "LISTEN" && $4 ~ ":${elasticsearch_port}"')
+if [[ -z "${RESULT// }" ]] ; then
+	nohup ${ELASTICSEARCH_BIN} >/dev/null 2>&1 &
+	echo $! > ${TMP_DIR}/elasticsearch.pid
+else
+    echo "> port ${elasticsearch_port} already bound by another process. We assume that Elasticsearch is already running."
+fi;
+
+let "j=0";
+HTTP_STATUS=$(curl -s -w %{http_code} localhost:${elasticsearch_port})
+
+while [[ ${HTTP_STATUS} != *"200"* ]] && [ ${j} -lt 30 ] ; do
+	echo -n "$j. "; let "j += 1 ";  sleep 1;
+	HTTP_STATUS=$(curl -s -w %{http_code} localhost:${elasticsearch_port})
+done;
+if [ ${j} -ge 30 ]; then 
+	echo "[ERROR]: ElasticSearch doesn't started. "
+	exit;
+fi;
+
+echo ;
+if [[ ${HTTP_STATUS} != *"200"* ]]; then
+    echo "> Elasticsearch is unreachable. Aborting."
+    exit 1;
+fi;
+echo "Done. Elasticsearch started successfully on port 9400.";
+echo
+
+echo "Starting the monitoring server ...";
+NODE_DIR=${DIST_DIR}/nodejs/bin
+NODE_BIN=${NODE_DIR}/node
+NPM_BIN=${DIST_DIR}/nodejs/bin/npm
+export PATH=${NODE_DIR}:${PATH}
+
+command -v ${NODE_BIN} >/dev/null 2>&1 || { echo " node  : Not installed. Aborting.\n" >&2; exit 1; }
+command -v ${NPM_BIN} >/dev/null 2>&1 || { echo " npm  : Not installed. Aborting.\n" >&2; exit 1; }
+
+${NPM_BIN} install
+nohup ./bin/www_with_repository >/dev/null 2>&1 &
+echo $! > ${TMP_DIR}/node.pid
+echo "Done. Server is listening on port ${nodejs_port}.";
+echo

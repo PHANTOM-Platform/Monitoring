fa3582fea001e39852439c0d42ffb6fe38578a0f
Jose Miguel Montanana <jmmontanana@gmail.com>
2019-02-04 14:44:36 +0100

    Delete texto.c

--- Monitoring_client/src/api/src/texto.c
+++
@@ -1,6 +0,0 @@
-char parameters_name[9][32] = {"MAX_CPU_POWER", "MIN_CPU_POWER", 
-								"MEMORY_POWER", "L2CACHE_MISS_LATENCY", "L2CACHE_LINE_SIZE", 
-								"E_DISK_R_PER_KB", "E_DISK_W_PER_KB", 
-								"E_NET_SND_PER_KB", "E_NET_RCV_PER_KB"};
-								
-								

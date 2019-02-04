8591105ea02dfb3df6c17c8388e7aebe1650ea8b
Jose Miguel Montanana <jmmontanana@gmail.com>
2019-02-04 14:12:28 +0100

    Add files via upload

--- Monitoring_server/app.js
+++ Monitoring_server/app.js
@@ -7,10 +7,9 @@ var bodyParser = require('body-parser');
 var os = require("os");
 var elasticsearch = require('elasticsearch');
 var elastic = new elasticsearch.Client({
-  host: 'localhost:9400',
-  log: 'error'
+	host: 'localhost:9400',
+	log: 'error'
 });
-
 
 /* monitoring routes */
 var servername      = require('./routes/v1/servername');
@@ -37,14 +36,11 @@ app.set('view engine', 'jade');
 app.set('elastic', elastic);
 app.set('version', '23.06.18');
 var port = '3033',
-  hostname = os.hostname();
+hostname = os.hostname();
 // redirect backend hostname to front-end
 //hostname = hostname.replace('be.excess-project.eu', 'mf.excess-project.eu');
 app.set('mf_server', 'http://' + hostname + ':' + port + '/v1');
 //app.set('pwm_idx', 'power_dreamcloud');
-
-
-
 
 //*********************************************************
 function retrieve_file(filePath,res){
@@ -100,10 +96,9 @@ app.get('/monitoringserver.html', function(req, res) {
 	retrieve_file(filePath,res);
 });
 
-
-app.use(logger('combined', {
-  skip: function (req, res) { return res.statusCode < 400; }
-}));
+//app.use(logger('combined', {
+//	skip: function (req, res) { return res.statusCode < 400; }
+//}));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -122,7 +117,6 @@ app.use('/v1/phantom_mf/deployment',deployment); /*2017*/
 app.use('/v1/phantom_mf/deploymentcomm',deploymentcomm); /*2017*/
 app.use('/v1/phantom_mf/deploymentcomp',deploymentcomp); /*2017*/
 
-
 app.use('/v1/phantom_mf/metrics', metrics);
 app.use('/v1/phantom_mf/profiles', profiles);
 app.use('/v1/phantom_mf/runtime', runtime);
@@ -134,24 +128,25 @@ app.use('/v1/phantom_rm/configs', configs);
 
 /* catch 404 and forward to error handler */
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  next(err);
+	var err = new Error('Not Found');
+	next(err);
 });
 
 // development error handler
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    var error = {};
-    error.error = err;
-    res.json(error);
-  });
-}
+// if (app.get('env') === 'development') {
+// app.use(function(err, req, res, next) {
+// 	var error = {};
+// 	error.error = err;
+// 	res.json(error);
+// });
+// }
 
 // production error handler
-app.use(function(err, req, res, next) {
-  var error = {};
-  error.error = err;
-  res.json(error);
-});
+// app.use(function(err, req, res, next) {
+// 	var error = {};
+// 	error.error = err;
+// 	res.json(error);
+// });
 
 module.exports = app;
+

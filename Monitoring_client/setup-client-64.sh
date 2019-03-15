537ffee1a6aa52d501689b2caa41b07d26341713
Jose Miguel Montanana <jmmontanana@gmail.com>
2019-03-15 16:51:37 +0100

    Update setup-client-64.sh

--- Monitoring_client/setup-client-64.sh
+++ Monitoring_client/setup-client-64.sh
@@ -225,17 +225,17 @@
 # DOWNLOAD AND INSTALL lm_sensors                                            #
 # ============================================================================ #
 	cd $ROOT;
-	if [ ! -f lm_sensors-3.5.0.tar.gz ]; then
+	if [ ! -f lm-sensors-3-5-0.tar.gz ]; then
 		wget --no-check-certificate -4 https://fossies.org/linux/misc/lm-sensors-3-5-0.tar.gz;
 	fi;
-	if [ ! -f lm_sensors-3.5.0.tar.gz ]; then
-		echo "[ERROR] File not found: " lm_sensors-3.5.0.tar.gz;
+	if [ ! -f lm-sensors-3-5-0.tar.gz ]; then
+		echo "[ERROR] File not found: " lm-sensors-3-5-0.tar.gz;
 		exit 1;
 	fi;
-	tar zxvf lm_sensors-3.5.0.tar.gz;
-	cd lm_sensors-3.5.0
+	tar zxvf lm-sensors-3-5-0.tar.gz;
+	cd lm-sensors-3-5-0.tar.gz;
 	make PREFIX=${INSTALL_PATH_SENSORS} all
-	make PREFIX=${INSTALL_PATH_SENSORS} install
+	make PREFIX=${INSTALL_PATH_SENSORS} install	
 # ============================================================================ #
 # DOWNLOAD AND INSTALL LIBIIO                                                  #
 # https://wiki.analog.com/resources/tools-software/linux-software/libiio

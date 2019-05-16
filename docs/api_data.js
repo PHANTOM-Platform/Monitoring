5fae329268dd837bd4a08c5396bc5b6b6effb6ca
Jose Miguel Montanana <jmmontanana@gmail.com>
2019-05-16 17:24:04 +0200

    Add files via upload

---
+++ docs/api_data.js
@@ -0,0 +1,473 @@
+define({ "api": [
+  {
+    "type": "get",
+    "url": "/_flush",
+    "title": "It makes sure the data is stored in the database.",
+    "version": "1.0.0",
+    "name": "_flush",
+    "group": "Administration",
+    "permission": [
+      {
+        "name": "user at localhost"
+      }
+    ],
+    "description": "<p>It makes sure the data is stored in the database, then responses from other requests executed immediately after it will return data consistent with any update done before the flush.</p>",
+    "examples": [
+      {
+        "title": "Example usage:",
+        "content": "curl -s -H \"Authorization: OAuth ${mytoken}\" -XGET http://${server}:${port}/_flush\nor alternatively use service at https when available SSL certificates.",
+        "type": "json"
+      }
+    ],
+    "success": {
+      "examples": [
+        {
+          "title": "Success response:",
+          "content": "HTTPS 200 OK",
+          "type": "json"
+        }
+      ]
+    },
+    "error": {
+      "fields": {
+        "Error 4xx": [
+          {
+            "group": "Error 4xx",
+            "optional": false,
+            "field": "BadRequest",
+            "description": "<p>Report error message</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Response (example):",
+          "content": "HTTP/1.1 400 Bad Request",
+          "type": "json"
+        }
+      ]
+    },
+    "filename": "/home/jmontana/apidocs/server_code/execmanager_app.js",
+    "groupTitle": "Administration"
+  },
+  {
+    "type": "get",
+    "url": "/drop_db",
+    "title": "drops the database of the server",
+    "version": "1.0.0",
+    "name": "drop_db",
+    "group": "Administration",
+    "permission": [
+      {
+        "name": "user at localhost"
+      }
+    ],
+    "description": "<p>Deletes completely all the information stored at the database of server, it returns error if not exists the database to drop. Its use is restricted to request from the same machine where is running the server.</p>",
+    "examples": [
+      {
+        "title": "Example usage:",
+        "content": "curl -s -H \"Authorization: OAuth ${mytoken}\" -XGET http://${server}:${port}/drop_db\nor alternatively use service at https when available SSL certificates.",
+        "type": "json"
+      }
+    ],
+    "success": {
+      "examples": [
+        {
+          "title": "Success response:",
+          "content": "HTTPS 200 OK",
+          "type": "json"
+        }
+      ]
+    },
+    "error": {
+      "fields": {
+        "Error 4xx": [
+          {
+            "group": "Error 4xx",
+            "optional": false,
+            "field": "BadRequest",
+            "description": "<p>Report error message</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Response (example):",
+          "content": "HTTP/1.1 400 Bad Request",
+          "type": "json"
+        }
+      ]
+    },
+    "filename": "/home/jmontana/apidocs/server_code/execmanager_app.js",
+    "groupTitle": "Administration"
+  },
+  {
+    "type": "get",
+    "url": "/new_db",
+    "title": "Registers a new database of the server",
+    "version": "1.0.0",
+    "name": "new_db",
+    "group": "Administration",
+    "permission": [
+      {
+        "name": "user at localhost"
+      }
+    ],
+    "description": "<p>Registers a new database of the server, it may return error if already exists. Its use is restricted to request from the same machine where is running the server.</p>",
+    "examples": [
+      {
+        "title": "Example usage:",
+        "content": "curl -s -H \"Authorization: OAuth ${mytoken}\" -XGET http://${server}:${port}/new_db\nor alternatively use service at https when available SSL certificates.",
+        "type": "json"
+      }
+    ],
+    "success": {
+      "examples": [
+        {
+          "title": "Success response:",
+          "content": "HTTPS 200 OK",
+          "type": "json"
+        }
+      ]
+    },
+    "error": {
+      "fields": {
+        "Error 4xx": [
+          {
+            "group": "Error 4xx",
+            "optional": false,
+            "field": "BadRequest",
+            "description": "<p>Report error message</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Response (example):",
+          "content": "HTTP/1.1 400 Bad Request",
+          "type": "json"
+        }
+      ]
+    },
+    "filename": "/home/jmontana/apidocs/server_code/execmanager_app.js",
+    "groupTitle": "Administration"
+  },
+  {
+    "type": "post",
+    "url": "/signup",
+    "title": "Register a new user and its password.",
+    "version": "1.0.0",
+    "name": "signup",
+    "group": "Administration",
+    "permission": [
+      {
+        "name": "user"
+      }
+    ],
+    "description": "<p>Update the password of an user</p>",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "email",
+            "description": "<p>MANDATORY this is the user_id.</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "pw",
+            "description": "<p>MANDATORY it is the password of the user</p>"
+          }
+        ]
+      }
+    },
+    "examples": [
+      {
+        "title": "Example usage:",
+        "content": "curl -H \"Content-Type: text/plain\" -XPOST http://localhost:8000/signup?name=\"bob\"\\&email=\"bob@abc.commm\"\\&pw=\"1234\";\n \nor alternatively use service at https when available SSL certificates.",
+        "type": "json"
+      }
+    ],
+    "success": {
+      "examples": [
+        {
+          "title": "Success response:",
+          "content": "HTTPS 200 OK",
+          "type": "json"
+        }
+      ]
+    },
+    "error": {
+      "examples": [
+        {
+          "title": "Response when missing mandatory parameters:",
+          "content": "HTTP/1.1 400 Bad Request",
+          "type": "json"
+        },
+        {
+          "title": "Response when not valid user/password:",
+          "content": "HTTP/1.1 401 Not Authenticated",
+          "type": "json"
+        }
+      ]
+    },
+    "filename": "/home/jmontana/apidocs/server_code/execmanager_app.js",
+    "groupTitle": "Administration"
+  },
+  {
+    "type": "post",
+    "url": "/update_user",
+    "title": "Update the password of an user",
+    "version": "1.0.0",
+    "name": "update_user",
+    "group": "Administration",
+    "permission": [
+      {
+        "name": "user"
+      }
+    ],
+    "description": "<p>Update the password of an user</p>",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "email",
+            "description": "<p>MANDATORY this is the user_id.</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "pw",
+            "description": "<p>MANDATORY it is the password of the user</p>"
+          }
+        ]
+      }
+    },
+    "examples": [
+      {
+        "title": "Example usage:",
+        "content": "curl -H \"Content-Type: text/plain\" -XPOST http://localhost:8000/update_user?name=\"bob\"\\&email=\"bob@abc.commm\"\\&pw=\"1234\";\n \nor alternatively use service at https when available SSL certificates.",
+        "type": "json"
+      }
+    ],
+    "success": {
+      "examples": [
+        {
+          "title": "Success response:",
+          "content": "HTTPS 200 OK",
+          "type": "json"
+        }
+      ]
+    },
+    "error": {
+      "examples": [
+        {
+          "title": "Response when missing mandatory parameters:",
+          "content": "HTTP/1.1 400 Bad Request",
+          "type": "json"
+        },
+        {
+          "title": "Response when not valid user/password:",
+          "content": "HTTP/1.1 401 Not Authenticated",
+          "type": "json"
+        }
+      ]
+    },
+    "filename": "/home/jmontana/apidocs/server_code/execmanager_app.js",
+    "groupTitle": "Administration"
+  },
+  {
+    "type": "post",
+    "url": "/login",
+    "title": "Returns a new token",
+    "version": "1.0.0",
+    "name": "login",
+    "group": "Security",
+    "permission": [
+      {
+        "name": "user"
+      }
+    ],
+    "description": "<p>Returns a new encrypted token, with a limited lifetime, for the user_id if the user_id/password provided are valid.</p>",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "email",
+            "description": "<p>MANDATORY this is the user_id.</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "pw",
+            "description": "<p>MANDATORY it is the password of the user</p>"
+          }
+        ]
+      }
+    },
+    "examples": [
+      {
+        "title": "Example usage:",
+        "content": "curl -H \"Content-Type: text/plain\" -XGET http://localhost:8000/login?email=\"bob\"\\&pw=\"1234\" --output token.txt;\n \nor alternatively use service at https when available SSL certificates.",
+        "type": "json"
+      }
+    ],
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "response",
+            "description": "<p>token which consists on a encrypted text string.</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Success response:",
+          "content": "HTTPS 200 OK",
+          "type": "json"
+        }
+      ]
+    },
+    "error": {
+      "examples": [
+        {
+          "title": "Response when missing mandatory parameters:",
+          "content": "HTTP/1.1 400 Bad Request",
+          "type": "json"
+        },
+        {
+          "title": "Response when not valid user/password:",
+          "content": "HTTP/1.1 401 Not Authenticated",
+          "type": "json"
+        }
+      ]
+    },
+    "filename": "/home/jmontana/apidocs/server_code/execmanager_app.js",
+    "groupTitle": "Security"
+  },
+  {
+    "type": "get",
+    "url": "/verifytoken",
+    "title": "Returns if the provided Token is valid or not",
+    "version": "1.0.0",
+    "name": "verifytoken",
+    "group": "Security",
+    "permission": [
+      {
+        "name": "user"
+      }
+    ],
+    "description": "<p>Verifies of the provided token is valid. Authorization Tokens are generated when user autenticates with an id and a password, the tokens are required for accessing to private content only if autenticated.</p>",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "the",
+            "description": "<p>token.</p>"
+          }
+        ]
+      }
+    },
+    "examples": [
+      {
+        "title": "Example usage:",
+        "content": "curl -s -H \"Authorization: OAuth ${mytoken}\" -XGET http://${server}:${port}/verifytoken\nor alternatively when available SSL certificates:\ncurl -s -H \"Authorization: OAuth ${mytoken}\" -XGET https://${server}:${port}/verifytoken",
+        "type": "json"
+      }
+    ],
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "reponse",
+            "description": "<p>The token is valid !!!.</p>"
+          }
+        ]
+      }
+    },
+    "error": {
+      "fields": {
+        "Error 4xx": [
+          {
+            "group": "Error 4xx",
+            "optional": false,
+            "field": "InvalidToken",
+            "description": "<p>Invalid token</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Response (example):",
+          "content": "HTTP/1.1 401 Not Authenticated\n  Invalid token",
+          "type": "json"
+        }
+      ]
+    },
+    "filename": "/home/jmontana/apidocs/server_code/execmanager_app.js",
+    "groupTitle": "Security"
+  },
+  {
+    "type": "get",
+    "url": "/verify_es_connection",
+    "title": "Returns success (200) when the Server has connection or server-error(503) in other case",
+    "version": "1.0.0",
+    "name": "verify_es_connection",
+    "group": "Testing_Functionality",
+    "permission": [
+      {
+        "name": "user"
+      }
+    ],
+    "description": "<p>The purpose is use for verifying the connectivity of the server with the ElasticSearch Database.</p>",
+    "examples": [
+      {
+        "title": "Example usage:",
+        "content": "curl -s -XGET http://${server}:${port}/verify_es_connection\nor alternatively use service at https when available SSL certificates.",
+        "type": "json"
+      }
+    ],
+    "success": {
+      "examples": [
+        {
+          "title": "Success response:",
+          "content": "HTTPS 200 OK",
+          "type": "json"
+        }
+      ]
+    },
+    "error": {
+      "examples": [
+        {
+          "title": "Response (example):",
+          "content": "HTTP/1.1 503 Service Unavailable",
+          "type": "json"
+        }
+      ]
+    },
+    "filename": "/home/jmontana/apidocs/server_code/execmanager_app.js",
+    "groupTitle": "Testing_Functionality"
+  }
+] });
---
+++ docs/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "PHANTOM Execution Manager API",
+  "version": "1.0.0",
+  "description": "...",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-05-16T14:45:27.488Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
